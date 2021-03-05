03897033cdd5b32ffdca2ff3cbd4150483e2d1cc
Metehan Gültekin <mgmetehan@hotmail.com>
2021-03-05 21:55:45 +0300

    Update UzayOyunu.java

--- UzayOyunu/src/UzayOyunu.java
+++ UzayOyunu/src/UzayOyunu.java
@@ -2,27 +2,28 @@ import java.awt.Color;
 import java.awt.HeadlessException;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+
 //Main Class
 public class UzayOyunu extends JFrame {
-	public UzayOyunu(String title) throws HeadlessException {// Title Yaratmak için
+	public UzayOyunu(String title) throws HeadlessException {// Title Yaratmak icin
 		super(title);
 		ImageIcon logo = new ImageIcon(UzayOyunu.class.getResource("/img/icon.png"));
-		setIconImage(logo.getImage());// icon resmini deðiþtirir
+		setIconImage(logo.getImage());// icon resmini degistirir
 	}
 
 	public static void main(String[] args) {
 		UzayOyunu ekran = new UzayOyunu("Uzay Oyunu");
 		ekran.setResizable(false);
-		ekran.setFocusable(false);// frame deðil jpanele focuslanacak
+		ekran.setFocusable(false);// frame degil jpanele focuslanacak
 		ekran.setSize(800, 600);
 		ekran.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		OyunPanel oyun = new OyunPanel();
-		
-		// Bunlarýn sýrasý önemli
-		oyun.requestFocus();// Jpanelin bizim klavye iþlemlerini anlamasý için fokusu ona veriyoruz
+
+		// Bunlarin sirasi onemli
+		oyun.requestFocus();// Jpanelin bizim klavye islemlerini anlamasi icin fokusu ona veriyoruz
 		oyun.addKeyListener(oyun);
-		oyun.setFocusable(true);// odaðý jframeden çekmiþtik artýk odaðý jpanele verdik
-		oyun.setFocusTraversalKeysEnabled(false);// klavye iþlemlerinin jpanelinin anlmasý için gerekli
+		oyun.setFocusable(true);// odagi jframeden cekmistik artik odagi jpanele verdik
+		oyun.setFocusTraversalKeysEnabled(false);// klavye islemlerinin jpanelinin anlmasi icin gerekli
 		ekran.add(oyun);
 		ekran.setVisible(true);
 	}
